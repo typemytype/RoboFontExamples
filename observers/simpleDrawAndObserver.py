097344c98d26d645174e7be5667b44a9c8996fef
Frederik Berlaen <frederik@typemytype.com>
2012-03-23 20:10:41 +0100

    draw  and  observer example

---
+++ observers/simpleDrawAndObserver.py
@@ -0,0 +1,13 @@
+from mojo.drawingTools import *
+from mojo.events import addObserver
+
+class DrawTest:
+    def __init__(self):
+        ## add an observer for the draw event
+        addObserver(self, "drawSomething", "draw")
+
+    def drawSomething(self, glyph, info):
+        ## draw something the glyph view
+        rect(100, 100, 100, 100)
+
+DrawTest()
