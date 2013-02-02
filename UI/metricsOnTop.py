5f9b717acdd00f75b2d93ab8b8ccf8fcd40d98fe
Frederik Berlaen <frederik@typemytype.com>
2013-02-02 13:24:55 +0100

    adding nice comments

--- UI/metricsOnTop.py
+++ UI/metricsOnTop.py
@@ -2,18 +2,23 @@ from mojo.events import addObserver
 
 class MetricsOnTop(object):
     
-    def __init__(self):    
+    def __init__(self):
+        # add an obserer when a space center did open
         addObserver(self, "metricsOnTop", "spaceCenterDidOpen")
     
     def metricsOnTop(self, notification):
+        # get the window
         window = notification["window"]
+        # get the space center object
         spaceCenter = window.getSpaceCenter()
-
+        
+        # vanilla rocks!
+        # get the position of each element
         l, t, r, b = spaceCenter.glyphLineView.getPosSize()
         ll, tt, rr, bb = spaceCenter.inputScrollView.getPosSize()
 
         tt = abs(tt)
-
+        # and use the old positions to reorganise the views
         spaceCenter.glyphLineView.setPosSize((l, t+tt+1, r, -0))
         spaceCenter.inputScrollView.setPosSize((ll, t, rr, bb))
         spaceCenter.hl.setPosSize((l, t+tt, 0, 1))
