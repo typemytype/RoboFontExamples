853f35ffb89e0aab7856a1141e3ced43cadc25a1
Frederik Berlaen <frederik@typemytype.com>
2014-02-14 15:35:07 +0100

    adding menu item example

---
+++ UI/buildMenu.py
@@ -0,0 +1,31 @@
+from AppKit import *
+from vanilla.vanillaBase import VanillaCallbackWrapper
+
+class BuildMenu(object):
+    
+    def __init__(self):
+        
+        myMenuTitle = "TypeMedia Awsome scripts"
+        
+        # create a new menu
+        myMenu = NSMenu.alloc().initWithTitle_(myMenuTitle)
+        # create a new item
+        item = myMenu.addItemWithTitle_action_keyEquivalent_("do it", "action:", "")
+        # create a callback
+        self.callbackwrapper = VanillaCallbackWrapper(self.doIt)
+        # set the callback
+        item.setTarget_(self.callbackwrapper)
+        
+        
+        # ask the main menu
+        menu = NSApp().mainMenu()
+        # add item
+        newItem = menu.addItemWithTitle_action_keyEquivalent_(myMenuTitle, "", "")
+        # set submenu
+        newItem.setSubmenu_(myMenu)
+    
+    
+    def doIt(self, sender):
+        print "hello"
+        
+BuildMenu()
