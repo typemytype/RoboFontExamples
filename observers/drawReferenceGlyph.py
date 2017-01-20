739fd6bb95cc4a3a59d28da8610ebeda8bad87f8
Frederik Berlaen <frederik@typemytype.com>
2017-01-20 12:42:01 +0100

    getting char from unicode with embedded unicodeToChar

--- observers/drawReferenceGlyph.py
+++ observers/drawReferenceGlyph.py
@@ -1,5 +1,5 @@
 """
-An example script of adding an observers and do *something* 
+An example script of adding an observers and do *something*
 
 It draws a simple unicode reference of an existing installed font.
 """
@@ -7,23 +7,24 @@ It draws a simple unicode reference of an existing installed font.
 from mojo.events import addObserver
 from mojo.drawingTools import *
 
+from lib.tools.misc import unicodeToChar
+
+
 class DrawReferenceGlyph(object):
-    
+
     def __init__(self):
         addObserver(self, "drawReferenceGlyph", "draw")
 
     def drawReferenceGlyph(self, info):
-        
         glyph = info["glyph"]
         scaleValue = info["scale"]
         r = 0
         g = 0
         b = 0
         a = .5
-        
-        if glyph is not None and glyph.unicode is not None and glyph.unicode < 0xFFFF:
-            t = unichr(glyph.unicode)
-            
+
+        if glyph is not None and glyph.unicode is not None:
+            t = unicodeToChar(glyph.unicode)
             save()
             translate(glyph.width + 10, 10)
             scale(scaleValue)
@@ -32,6 +33,6 @@ class DrawReferenceGlyph(object):
             fill(r, g, b, a)
             text(t, (0, 0))
             restore()
-            
-            
+
+
 DrawReferenceGlyph()
