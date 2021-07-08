cc5668de3d27ce996cff5eadb32d0b905c2aff65
Frederik Berlaen <frederik@typemytype.com>
2021-07-08 11:51:39 +0200

    use the same example as in the docs

--- observers/drawReferenceGlyph.py
+++ observers/drawReferenceGlyph.py
@@ -6,36 +6,50 @@ It draws a simple unicode reference of an existing installed font.
 from mojo.subscriber import Subscriber, registerGlyphEditorSubscriber
 
 
-class ReferenceGlyph(Subscriber):
+class DrawReferenceGlyph(Subscriber):
 
     debug = True
 
     def build(self):
         glyphEditor = self.getGlyphEditor()
         self.container = glyphEditor.extensionContainer(
-            identifier="com.typemytype.unicodePreview",
-            location="background",
+            identifier="com.roboFont.DrawReferenceGlyph.foreground",
+            location="foreground",
             clear=True)
 
-        self.unicodeText = self.container.appendTextLineSublayer(
-            fillColor=(0, 0, 0, .5),
-            offset=(10, 0),
-            font="Georgia",
+    def started(self):
+        self.textLineLayer = self.container.appendTextLineSublayer(
             pointSize=30,
-            verticalAlignment="top"
+            font='Georgia',
+            text="",
+            fillColor=(0, 0, 0, 0.4),
+            horizontalAlignment="left"
         )
+
+    def destroy(self):
+        self.container.clearSublayers()
 
     def glyphEditorDidSetGlyph(self, info):
         glyph = info["glyph"]
-        text = ""
-        if glyph.unicode:
-            text = chr(glyph.unicode)
-        self.unicodeText.setText(text)
-        self.unicodeText.setPosition((glyph.width, 0))
+        if glyph is None:
+            self.textLineLayer.setText("")
+            return
+
+        txt = ""
+        if glyph.unicode is not None:
+            # get character for glyph
+            txt = chr(glyph.unicode)
+        self.textLineLayer.setPosition((glyph.width, 0))
+        self.textLineLayer.setText(txt)
 
     def glyphEditorGlyphDidChangeMetrics(self, info):
         glyph = info["glyph"]
-        self.unicodeText.setPosition((glyph.width, 0))
+        if glyph is None:
+            self.textLineLayer.setText("")
+            return
+        self.textLineLayer.setPosition((glyph.width, 0))
 
 
-registerGlyphEditorSubscriber(ReferenceGlyph)
+if __name__ == '__main__':
+    registerGlyphEditorSubscriber(DrawReferenceGlyph)
+
